71873be67af4c82e1a56177839a67c2edaa851f2
Kittisak106 <43182617+Kittisak106@users.noreply.github.com>
2018-11-27 14:33:32 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -3,7 +3,7 @@
 
   function pubMqtt($topic,$msg){
        
-      put("https://api.netpie.io/topic/PIE/$topic?retain",$msg);
+      put("https://api.netpie.io/topic/Unbalance/$topic?retain",$msg);
  
   }
   function getMqttfromlineMsg($lineMsg){
